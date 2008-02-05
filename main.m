b8e29ae68eeac6e028a2bd58dc00682315fe8b86
Pete Yandell <pete@notahat.com>
2008-02-05 06:00:00 +0000

    Took out all the old license info from the tops of the source files.
    git-svn-id: http://svn.notahat.com/simplesynth/trunk@24 1d86d8c0-9bdf-0310-887a-a6f589a89adc

--- main.m
+++ main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  SimpleSynth
-//
-//  Created by Peter Yandell on 31/05/06.
-//  Copyright __MyCompanyName__ 2006. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
